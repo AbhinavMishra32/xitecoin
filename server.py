f7122db3eb1600e44a97d67aa3332a668a8125a0
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-01-11 23:21:52 +0530

    Update server port and handle connection errors

--- server.py
+++ server.py
@@ -4,14 +4,16 @@ import threading
 import json
 import time
 
+from client import HOST
+
 HEADER = 64
-PORT = 5000
+PORT = 3000
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
-known_nodes = ['localhost:5000']
+known_nodes = ['localhost:3000']
 def handle_client(conn, addr):
 	global known_nodes
 	data = conn.recv(1024)
@@ -21,6 +23,7 @@ def handle_client(conn, addr):
 
 def start_server():
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	server.bind(('localhost', PORT))
 	server.listen()
 	while True:
@@ -28,16 +31,18 @@ def start_server():
 		handle_client(conn, addr)
 		
 def connect_to_node(node):
-    global known_nodes
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host, port = node.split(':')
-    client.connect((host, int(port)))
-    data = json.dumps(known_nodes).encode(FORMAT)
-    client.send(data)
+    try:
+        global known_nodes
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host, port = node.split(':')
+        client.connect((host, int(port)))
+        data = json.dumps(known_nodes).encode(FORMAT)
+        client.send(data)
+    except socket.error as e:
+        print(f"Error occured: {e}")
 
 server_thread = threading.Thread(target = start_server)
 server_thread.start()
-
 # time.sleep(1)
 
 # connect_to_node('localhost:5000')
