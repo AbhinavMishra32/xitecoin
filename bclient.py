d83705da6f4949801b047682baa77df7344b1a99
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2024-01-13 22:26:11 +0530

    Add socket communication functionality to BClient and BServer classes

--- bclient.py
+++ bclient.py
@@ -2,7 +2,19 @@ import socket
 import json
 import threading
 
+HOST = socket.gethostbyname(socket.gethostname()) #this is the bootstrap server's ip
+PORT = 12345
+ADDR = ("HOST", PORT)
 
 class BClient():
 	def __init__(self):
-		
+		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.client.connect(ADDR)
+
+	def send_data(self):
+		# data = json.loads(json.dumps({"test": "message"}))
+		# self.client.sendall()
+		pass
+	
+	def recieve_msg(self):
+		self.client.
--- bootstrap_server.py
+++ bootstrap_server.py
@@ -16,31 +16,45 @@ PORT = 12345
 class BServer():
 	def __init__(self, host, port):
 		self.active_peers = []
+		self.active_peers_adrs = []
 		self.host = host
 		self.port = port
 
-	def handle_client(self, client, address):
-		print(f"Handling client: {address}")
-		while True:
-			data = client.recv(1024)
-			if not data:
-				print(f"Connection closed by: {address}")
-				self.active_peers.remove(address)
-				client.close()
-				break
-			print(f"Recieved data from {address}: {data.decode()}")
+	def broadcast(self, message):
+		for client in self.active_peers:
+			client.sendall(message.encode())
+
+	def handle_client(self, client: socket.socket, address):
+		try:	
+			print(f"Handling client: {address}")
+			while True:
+				client.send(json.dumps(self.active_peers_adrs).encode())
+				data = client.recv(1024)
+				if not data:
+					print(f"Connection closed by: {address}")
+					self.active_peers.remove(address)
+					client.close()
+					break
+				print(f"Recieved data from {address}: {data.decode()}")
+		except Exception as e:
+			print(f"Error occured: {e}")
 
 	def start(self):
-		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		server.bind((self.host, self.port))
-		server.listen()
-		print(f"Server started on {HOST}:{PORT}")
-		while True:
-			client, address = server.accept()
-			print(f"Connected with {address}")
-			self.active_peers.append(address)
-			client_thread = threading.Thread(target = self.handle_client, args = (client, address))
-			client_thread.start()
+		try:
+			server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			server.bind((self.host, self.port))
+			server.listen()
+			print(f"Server started on {self.host}:{self.port}")
+			while True:
+				client, address = server.accept()
+				new_client = {"client": {address}}
+				self.active_peers_adrs.append(new_client)
+				print(f"Connected with {address}")
+				self.active_peers.append(client)
+				client_thread = threading.Thread(target = self.handle_client, args = (client, address))
+				client_thread.start()
+		except Exception as e:
+			print(f"Error occured: {e}")
 
 xite_bserv = BServer(HOST, PORT)
 xite_bserv.start()
